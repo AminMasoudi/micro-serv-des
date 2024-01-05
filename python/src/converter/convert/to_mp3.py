11f4d1ba625cfccb375c081fe7c3090dcec1a82f
AminMasoudi <aminmasoudi2003@gmail.com>
2024-01-05 20:07:21 +0330

    IMP => converter

---
+++ python/src/converter/convert/to_mp3.py
@@ -0,0 +1,41 @@
+import pika, json, tempfile, os
+from bson.objectid import ObjectId
+import moviepy.editor
+
+def start(message, fs_video, fs_mp3, channel):
+    message = json.loads(message)
+    
+    tf = tempfile.NamedTemporaryFile()
+    out = fs_video.get(ObjectId(message["video_fid"]))
+    tf.write(out.read())
+    #convert
+    audio = moviepy.editor.VideoFileClip(tf.name).audio
+    tf.close()
+    
+    #write
+    tf_path = tempfile.gettempdir() + f"/{message['video_fid']}.mp3"
+    audio.write_audiofile(tf_path)
+
+    f = open(tf_path, "rb")
+    data = f.read()
+    f_id = fs_mp3s.put(data)
+    f.close()
+    os.remove(tf_path)
+
+    message["mp3_fid"] = str(fid)
+
+    try:
+        channel.basic_publish(
+                exchange="",
+                routing_key=os.environ.get("MP3_QUEUE"),
+                body=json.dumps(message),
+                properties=pika.BasicProperties(
+                    delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
+                ),
+        )
+    except :
+        fs_mp3.delete(fid)
+        return "failed to publish message"
+
+
+
