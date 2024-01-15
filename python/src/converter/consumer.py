671bdf6b18931b183762d68696d9d7e811d3307f
AminMasoudi <aminmasoudi2003@gmail.com>
2024-01-15 13:56:00 +0330

    FIX => host

--- python/src/converter/consumer.py
+++ python/src/converter/consumer.py
@@ -4,7 +4,7 @@ import gridfs
 from convert import to_mp3
 
 def main():
-    client = MongoClient("host.minikube.internal", 27017)
+    client = MongoClient("BlackHoleSun", 27017)
     db_videos   = client.videos
     db_mp3s     = client.mp3s
 
